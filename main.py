8d4a96137c9a3e303cb73b92e875085e111cb974
Stefan <stefan.egger@posteo.de>
2019-12-02 19:43:17 +0100

    First Jinja and Flexbox added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, make_response
 
 #wir legen app Objekt an mit dem wir die Webbanwendung kunfigurieren könnne
 app = Flask(__name__)
@@ -16,7 +16,19 @@ def index ():
 
 @app.route("/about_me")
 def about_me ():
-    return render_template("about_me.html")
+    #blabla = ["Hello 1", "Hello 2", "Hey im here"]
+    blabla = range(1000)
+    print(request.headers)
+    response = render_template("about_me.html", foo=blabla)
+    return response
+
+@app.route("/contact")
+def contact ():
+    name = request.args.get("name")
+    response = make_response(render_template("contact.html"))
+    if name is not None:
+        response.set_cookie("name", name)
+    return response
 
 @app.route("/portfolio")
 def portfolio ():
@@ -37,4 +49,4 @@ def hairdersser_hans ():
 
 #wenn dieses Modul das Hauptmodul ist, dann satrten wir flask
 if __name__=="__main__":
-    app.run()
+    app.run(debug=True)
